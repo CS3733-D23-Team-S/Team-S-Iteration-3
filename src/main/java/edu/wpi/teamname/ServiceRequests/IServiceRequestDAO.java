8d5f30d7eac0bf9ea03e6607ef15e5e19825489a
Fatducky1 <NathanielRubinM@gmail.com>
2023-04-09 17:57:35 -0400

    Added IServiceRequestDAO

--- src/main/java/edu/wpi/teamname/ServiceRequests/IServiceRequestDAO.java
+++ src/main/java/edu/wpi/teamname/ServiceRequests/IServiceRequestDAO.java
@@ -2,6 +2,9 @@ package edu.wpi.teamname.ServiceRequests;
 
 import edu.wpi.teamname.databaseredo.IDAO;
 
+import java.util.List;
+
 public interface IServiceRequestDAO extends IDAO {
+    public List<String> getListOfEligibleRooms();
 
 }
