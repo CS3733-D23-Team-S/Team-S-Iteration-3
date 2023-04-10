ee99af80ed59afec01512929ed851e90f0fcc653
TBrown2024 <tbrown@wpi.edu>
2023-04-10 16:58:16 -0400

    Edited Main to Work With New Databas

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,16 +1,13 @@
 package edu.wpi.teamname;
 
 import edu.wpi.teamname.databaseredo.DataBaseRepository;
-import edu.wpi.teamname.Database.LoaderDAO;
 import java.sql.SQLException;
 
 public class Main {
 
   public static void main(String[] args) throws SQLException {
-    LoaderDAO loader = LoaderDAO.getInstance();
-    //    loader.establishConnection();
-    //    loader.resetData();
-    loader.load();
+    DataBaseRepository database = DataBaseRepository.getInstance();
+    database.load();
     App.launch(App.class, args);
     // Debugging stuff in order to check everything looks about right
     System.out.println("Loaded everything");
