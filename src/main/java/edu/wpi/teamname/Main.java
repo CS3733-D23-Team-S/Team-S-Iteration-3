49dae69c3fd199ea9efc24c8ca08597654569c6d
Jake O <olsenja463@gmail.com>
2023-04-10 18:12:18 -0400

    cleaned up a bit

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -12,14 +12,5 @@ public class Main {
     App.launch(App.class, args);
     dbConnection.getInstance().getConnection().close();
     System.out.println("Loaded everything");
-    //    for (int key : NodeDaoImpl.getInstance().getNodes().keySet())
-    //      System.out.println(NodeDaoImpl.getInstance().getNodes().get(key).toString());
-    //    for (String key : MoveDaoImpl.getInstance().getMoves().keySet())
-    //      System.out.println(MoveDaoImpl.getInstance().getMoves().get(key).toString());
-    //    for (int key : EdgeDaoImpl.getInstance().getNeighbors().keySet()) {
-    //      System.out.print(key);
-    //      System.out.print("\t Neighbors:\t");
-    //      System.out.println(EdgeDaoImpl.getInstance().getNeighbors().get(key).toString());
-    //    }
   }
 }
