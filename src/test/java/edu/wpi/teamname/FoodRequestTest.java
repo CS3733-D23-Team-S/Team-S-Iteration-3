91f5016e8703af30b31b7b1ee5a1332d2c73e29f
Anthony <atitcombe13@gmail.com>
2023-04-12 00:08:58 -0400

    Test setup

---
+++ src/test/java/edu/wpi/teamname/FoodRequestTest.java
@@ -0,0 +1,17 @@
+package edu.wpi.teamname;
+
+import edu.wpi.teamname.ServiceRequests.FoodService.Food;
+import edu.wpi.teamname.ServiceRequests.FoodService.FoodDelivery;
+import edu.wpi.teamname.ServiceRequests.FoodService.FoodDeliveryDAOImp;
+import org.junit.jupiter.api.Test;
+
+public class FoodRequestTest {
+
+    FoodDeliveryDAOImp foodDeliveryDAO = new FoodDeliveryDAOImp();
+    FoodDelivery foodDelivery1 = null;
+
+    Food food1 = null;
+
+
+
+}
