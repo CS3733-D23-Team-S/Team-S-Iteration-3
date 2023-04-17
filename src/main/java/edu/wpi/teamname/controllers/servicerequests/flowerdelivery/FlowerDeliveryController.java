f39b8202db24a262a1df75a215799fac2184b6d5
Nicholas Ho <NicholasHo385@gmail.com>
2023-04-17 02:49:17 -0400

    updates to flower cart page

--- src/main/java/edu/wpi/teamname/controllers/servicerequests/flowerdelivery/FlowerDeliveryController.java
+++ src/main/java/edu/wpi/teamname/controllers/servicerequests/flowerdelivery/FlowerDeliveryController.java
@@ -8,34 +8,15 @@ import edu.wpi.teamname.ServiceRequests.flowers.Flower;
 import edu.wpi.teamname.navigation.Navigation;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
-import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 
 public class FlowerDeliveryController {
   public static int flowerID;
-
-  @FXML ImageView backicon;
-  @FXML ImageView exiticon;
-  @FXML MFXButton flower1;
-  @FXML MFXButton flower10;
-  @FXML MFXButton flower2;
-  @FXML MFXButton flower3;
-  @FXML MFXButton flower4;
-  @FXML MFXButton flower5;
-  @FXML MFXButton flower6;
-  @FXML MFXButton flower7;
-  @FXML MFXButton flower8;
-  @FXML MFXButton flower9;
-  @FXML MFXButton flowerbutton;
-  @FXML MFXTextField flowersearch;
-  @FXML HBox hbox1;
-  @FXML HBox hbox2;
   @FXML MenuButton sizedrop;
   @FXML MFXButton viewcartbutton;
   @FXML MenuItem sizesmall;
@@ -61,57 +42,90 @@ public class FlowerDeliveryController {
 
   public void filterSmall() {
     flowpane.getChildren().clear();
-    for (int i = 0; i < dbr.getFlowerDAO().getListOfSize("small").size(); i++) {
+    for (Flower f : dbr.getFlowerDAO().getListOfSize("small")) {
+      Image image = new Image(Main.class.getResource(f.getImage()).toString());
+      ImageView view = new ImageView(image);
+      view.setPreserveRatio(true);
+      view.setFitHeight(150);
+      view.setFitWidth(150);
+
       MFXButton btn1 = new MFXButton();
-      btn1.setId(dbr.getFlowerDAO().getListOfSize("small").get(i).toString());
-      btn1.setText(dbr.getFlowerDAO().getListOfSize("small").get(i).getName());
-      btn1.setPrefWidth(200);
+      btn1.setId(f.toString());
+      btn1.setText(f.getName());
+      btn1.setPrefWidth(250);
       btn1.setPrefHeight(200);
+      btn1.setStyle("-fx-background-radius:10 10 10 10;");
+      btn1.setWrapText(true);
+      btn1.setGraphic(view);
+
       flowpane.getChildren().add(btn1);
       flowpane.setHgap(20);
       flowpane.setVgap(20);
-      int finalII = i;
+
       btn1.setOnMouseClicked(
-          event -> store(dbr.getFlowerDAO().getListOfSize("small").get(finalII).getID()));
+          event -> {
+            store(f.getID());
+          });
     }
   }
 
   public void filterMedium() {
     flowpane.getChildren().clear();
-    for (int i = 0; i < dbr.getFlowerDAO().getListOfSize("medium").size(); i++) {
+    for (Flower f : dbr.getFlowerDAO().getListOfSize("medium")) {
+      Image image = new Image(Main.class.getResource(f.getImage()).toString());
+      ImageView view = new ImageView(image);
+      view.setPreserveRatio(true);
+      view.setFitHeight(150);
+      view.setFitWidth(150);
+
       MFXButton btn1 = new MFXButton();
-      btn1.setId(dbr.getFlowerDAO().getListOfSize("medium").get(i).toString());
-      btn1.setText(dbr.getFlowerDAO().getListOfSize("medium").get(i).getName());
-      btn1.setPrefWidth(200);
+      btn1.setId(f.toString());
+      btn1.setText(f.getName());
+      btn1.setPrefWidth(250);
       btn1.setPrefHeight(200);
+      btn1.setStyle("-fx-background-radius:10 10 10 10;");
+      btn1.setWrapText(true);
+      btn1.setGraphic(view);
+
       flowpane.getChildren().add(btn1);
       flowpane.setHgap(20);
       flowpane.setVgap(20);
-      int finalII = i;
+
       btn1.setOnMouseClicked(
-          event -> store(dbr.getFlowerDAO().getListOfSize("medium").get(finalII).getID()));
+          event -> {
+            store(f.getID());
+          });
     }
   }
 
   public void filterLarge() {
     flowpane.getChildren().clear();
-    for (int i = 0; i < dbr.getFlowerDAO().getListOfSize("large").size(); i++) {
-      Image image =
-          new Image(Main.class.getResource(dbr.getFlowerDAO().get(flowerID).getImage()).toString());
-      ImageView view = new ImageView();
+
+    flowpane.getChildren().clear();
+    for (Flower f : dbr.getFlowerDAO().getListOfSize("large")) {
+      Image image = new Image(Main.class.getResource(f.getImage()).toString());
+      ImageView view = new ImageView(image);
       view.setPreserveRatio(true);
+      view.setFitHeight(150);
+      view.setFitWidth(150);
+
       MFXButton btn1 = new MFXButton();
-      btn1.setId(dbr.getFlowerDAO().getListOfSize("large").get(i).toString());
-      btn1.setText(dbr.getFlowerDAO().getListOfSize("large").get(i).getName());
-      btn1.setPrefWidth(200);
+      btn1.setId(f.toString());
+      btn1.setText(f.getName());
+      btn1.setPrefWidth(250);
       btn1.setPrefHeight(200);
+      btn1.setStyle("-fx-background-radius:10 10 10 10;");
+      btn1.setWrapText(true);
       btn1.setGraphic(view);
+
       flowpane.getChildren().add(btn1);
       flowpane.setHgap(20);
       flowpane.setVgap(20);
-      int finalII = i;
+
       btn1.setOnMouseClicked(
-          event -> store(dbr.getFlowerDAO().getListOfSize("large").get(finalII).getID()));
+          event -> {
+            store(f.getID());
+          });
     }
   }
 
--- src/main/java/edu/wpi/teamname/controllers/servicerequests/flowerdelivery/FlowerPopupController.java
+++ src/main/java/edu/wpi/teamname/controllers/servicerequests/flowerdelivery/FlowerPopupController.java
@@ -1,7 +1,5 @@
 package edu.wpi.teamname.controllers.servicerequests.flowerdelivery;
 
-import static edu.wpi.teamname.controllers.servicerequests.flowerdelivery.FlowerDeliveryController.flowerID;
-
 import edu.wpi.teamname.DAOs.DataBaseRepository;
 import edu.wpi.teamname.Main;
 import edu.wpi.teamname.ServiceRequests.flowers.Cart;
@@ -30,20 +28,41 @@ public class FlowerPopupController extends PopUpController {
   public void initialize() {
     showInfo();
     addcartbutton.setOnMouseClicked(event -> createDelivery());
+    flowercounter = 1;
+    incrementbutton.setOnMouseClicked(event -> addquantity());
+    decrementbutton.setOnMouseClicked(event -> subtractquantity());
   }
 
   private void showInfo() {
-    flowername.setText(dbr.getFlowerDAO().get(flowerID).getName());
-    flowerdescription.setText(dbr.getFlowerDAO().get(flowerID).getDescription());
+    flowername.setText(dbr.getFlowerDAO().get(FlowerDeliveryController.flowerID).getName());
+    flowerdescription.setText(
+        dbr.getFlowerDAO().get(FlowerDeliveryController.flowerID).getDescription());
 
     Image image =
-        new Image(Main.class.getResource(dbr.getFlowerDAO().get(flowerID).getImage()).toString());
+        new Image(
+            Main.class
+                .getResource(dbr.getFlowerDAO().get(FlowerDeliveryController.flowerID).getImage())
+                .toString());
     flowerimage.setImage(image);
   }
 
   private void createDelivery() {
-    Flower flower = dbr.getFlowerDAO().get(flowerID);
-    flowerCart.addFlowerItem(flower);
+    System.out.println("added");
+    Flower flower = dbr.getFlowerDAO().get(FlowerDeliveryController.flowerID);
+    flower.setQuantity(flowercounter);
+    flowerCart.getCartItems().add(flower);
     stage.close();
+  }
+
+  public static int flowercounter;
+
+  public void addquantity() {
+    flowercounter++;
+    quantitylabel.setText(Integer.toString(flowercounter));
+  }
+
+  public void subtractquantity() {
+    flowercounter--;
+    quantitylabel.setText(Integer.toString(flowercounter));
   }
 }
