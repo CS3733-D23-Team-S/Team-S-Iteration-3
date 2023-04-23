3891fa8cf10e8c78b70d2176e0db27d6b470dc40
Anthony <atitcombe13@gmail.com>
2023-04-23 10:36:39 -0400

    pathfinding database

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,24 +1,25 @@
 package edu.wpi.teamname;
 
 import edu.wpi.teamname.DAOs.DataBaseRepository;
+import edu.wpi.teamname.DAOs.MoveDAOImpl;
 import edu.wpi.teamname.DAOs.dbConnection;
+import java.time.LocalDate;
 
 public class Main {
   static ConnectionThread thread;
 
   public static void main(String[] args) throws Exception {
+
     DataBaseRepository database = DataBaseRepository.getInstance();
     database.load();
-    thread = new ConnectionThread();
-    thread.start();
-    App.launch(App.class, args);
+    MoveDAOImpl moveDAO = database.getMoveDAO();
+    //    thread = new ConnectionThread();
+    //    thread.start();
+    moveDAO.constructForGivenDate(LocalDate.now());
+    moveDAO.getFutureMoves();
+    // App.launch(App.class, args);
     // dbConnection.getInstance().getConnection().close();
     System.out.println("Loaded everything");
     dbConnection.getInstance().getConnection().close();
-    //    for (int key : NodeDaoImpl.getInstance().getNodes().keySet())
-    //      System.out.println(NodeDaoImpl.getInstance().getNodes().get(key).toString());
-    //    for (String key : MoveDaoImpl.getInstance().getMoves().keySet())
-    //      System.out.println(MoveDaoImpl.getInstance().getMoves().get(key).toString());
-
   }
 }
