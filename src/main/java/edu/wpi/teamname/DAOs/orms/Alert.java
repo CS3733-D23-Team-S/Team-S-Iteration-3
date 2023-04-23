3313e7c01f7c63b0eb86353124f3518c25feddf0
Jake O <olsenja463@gmail.com>
2023-04-23 19:53:47 -0400

    implement database

--- src/main/java/edu/wpi/teamname/DAOs/orms/Alert.java
+++ src/main/java/edu/wpi/teamname/DAOs/orms/Alert.java
@@ -1,23 +1,17 @@
 package edu.wpi.teamname.DAOs.orms;
 
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 public class Alert {
-    @Getter @Setter String heading;
-    @Getter @Setter String message;
-    @Getter @Setter LocalDate dateOfAlert;
+  @Getter @Setter String heading;
+  @Getter @Setter String message;
+  @Getter @Setter LocalDate dateOfAlert;
 
-    public Alert(String heading, String message){
-        this.heading = heading;
-        this.message = message;
-        this.dateOfAlert = LocalDate.now();
-
-    }
-
-
-
-
+  public Alert(String heading, String message) {
+    this.heading = heading;
+    this.message = message;
+    this.dateOfAlert = LocalDate.now();
+  }
 }
