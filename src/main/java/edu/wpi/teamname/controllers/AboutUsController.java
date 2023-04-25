85c88638b340dfa9acaee92160ee16c39f266cd0
Anthony <atitcombe13@gmail.com>
2023-04-25 07:05:07 -0400

    aboutus

--- src/main/java/edu/wpi/teamname/controllers/AboutUsController.java
+++ src/main/java/edu/wpi/teamname/controllers/AboutUsController.java
@@ -1,4 +1,20 @@
 package edu.wpi.teamname.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class AboutUsController {
+
+    @FXML Text projManager;
+    @FXML Text prodOwner;
+    @FXML Text fullEng;
+    @FXML Text scrum;
+
+    public void initialize(){
+        projManager.setStyle("-fx-font-weight: BOLD");
+        prodOwner.setStyle("-fx-font-weight: bold");
+        fullEng.setStyle("-fx-font-weight: bold");
+        scrum.setStyle("-fx-font-weight: bold");
+
+    }
 }
