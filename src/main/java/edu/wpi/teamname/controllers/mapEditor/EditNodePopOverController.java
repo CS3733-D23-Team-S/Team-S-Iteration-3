27961657345093fdc33c12997e7dfaa4d93398da
Nikesh <nikeshwalling@gmail.com>
2023-04-25 19:31:18 -0400

    Niceness and map editor is goated

--- src/main/java/edu/wpi/teamname/controllers/mapEditor/EditNodePopOverController.java
+++ src/main/java/edu/wpi/teamname/controllers/mapEditor/EditNodePopOverController.java
@@ -9,6 +9,7 @@ import edu.wpi.teamname.DAOs.orms.Node;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import javafx.fxml.FXML;
@@ -16,6 +17,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import lombok.Setter;
+import org.controlsfx.control.PopOver;
 import org.controlsfx.control.SearchableComboBox;
 
 public class EditNodePopOverController {
@@ -27,7 +29,7 @@ public class EditNodePopOverController {
 	private VBox editMenu;
 
 	@FXML
-	private Button addEdge, addLocation, submitUpdate;
+	private Button addEdge, addLocation;
 	@FXML
 	private VBox edgeBox, locationBox, moveBox;
 	@FXML
@@ -43,11 +45,11 @@ public class EditNodePopOverController {
 	@FXML
 	private DatePicker dateSelect;
 	private boolean showClosest;
+	private HashMap<Label, Move> labelToMove = new HashMap<>();
+
 
 	Node node;
 
-	List<Integer> addedEdges = new ArrayList<>();
-	List<Move> addedMoves = new ArrayList<>();
 
 	public void initialize() {
 		for (Location loc : repo.getLocationDAO().getAll()) {
@@ -56,10 +58,11 @@ public class EditNodePopOverController {
 		addEdge.setOnMouseClicked(
 				event -> {
 					int id = edgeField.getSelectionModel().getSelectedItem();
-					if (repo.getNodeDAO().get(id) != null) {
-						edgeBox.getChildren().add(new Label(String.valueOf(id)));
-						addedEdges.add(id);
-						updateEdgeBox();
+					if (repo.getNode(id) != null) {
+						Label label = new Label(String.valueOf(id));
+						edgeBox.getChildren().add(label);
+						Edge newEdge = new Edge(repo.getNode(id), node);
+						mainController.queueManager.addToQueue(newEdge);
 					}
 				});
 		addLocation.setOnMouseClicked(
@@ -67,12 +70,12 @@ public class EditNodePopOverController {
 					String longName = locationField.getValue();
 					LocalDate date = dateSelect.getValue();
 					if (!longName.isEmpty()) {
-						if (repo.getMoveDAO().getLocationsAtNodeID().get(node.getNodeID()) != null)
-							for (Move move : repo.getMoveDAO().getLocationsAtNodeID().get(node.getNodeID()))
+						if (repo.getLocationsAtNode(node) != null)
+							for (Move move : repo.getLocationsAtNode(node))
 								if (longName.equals(move.getLocationName()) && date.isEqual(move.getDate())) return;
 					}
 					Move newMove = new Move(this.node, repo.getLocationDAO().get(longName), date);
-					addedMoves.add(newMove);
+					mainController.queueManager.addToQueue(newMove);
 					System.out.println("Added the move: " + newMove);
 					locationBox.getChildren().add(new Label(longName));
 					Label label =
@@ -86,38 +89,25 @@ public class EditNodePopOverController {
 					showClosest = checkBox.isSelected();
 					updateEdgeBox();
 				});
-		submitUpdate.setOnMouseClicked(
-				event -> { //					System.out.println("Tried to update");
-					for (int id : addedEdges) {
-						Edge newEdge = new Edge(this.node, repo.getNodeDAO().get(id));
-						System.out.println(newEdge);
-						repo.getEdgeDAO().add(newEdge);
-					}
-					for (Move move : addedMoves) {
-						System.out.println("adding the move " + move.toString());
-						repo.getMoveDAO().add(move);
-					}
-					if (node.getNodeID() != Integer.parseInt(nodeIDLabel.getText()))
-						repo.getNodeDAO()
-								.updateNodeID(this.node.getNodeID(), Integer.parseInt(nodeIDLabel.getText()));
-					this.node.setNodeID(Integer.parseInt(nodeIDLabel.getText()));
-					DataBaseRepository.getInstance().forceUpdate();
-				});
 	}
 
 	public void launchPopup(Circle circle) {
+		labelToMove.clear();
+		setInfo(mainController.listOfCircles.get(circle));
 		mainController.popOver.setContentNode(this.editMenu);
+		mainController.popOver.setTitle("Editing Node");
+		mainController.popOver.setArrowLocation(PopOver.ArrowLocation.LEFT_TOP);
 		mainController.popOver.show(circle);
 	}
 
 	private void update() {
 		edgeField.getItems().removeAll();
-		for (Node potentialEdge : repo.getNodeDAO().getAll()) {
+		for (Node potentialEdge : mainController.queueManager.getCurrentNodeChanges()) {
 			if (calcWeight(potentialEdge) < 60
 				&& !edgeField.getItems().contains(potentialEdge.getNodeID()))
 				edgeField.getItems().add(potentialEdge.getNodeID());
 		}
-		for (Location loc : repo.getLocationDAO().getAll()) {
+		for (Location loc : mainController.queueManager.getCurrentLocationChanges()) {
 			if (!locationField.getItems().contains(loc.getLongName()))
 				locationField.getItems().add(loc.getLongName());
 		}
@@ -133,21 +123,18 @@ public class EditNodePopOverController {
 		edgeField.getItems().removeAll();
 		if (!showClosest) {
 			edgeField.setPromptText("Nearest Nodes");
-			for (Node potentialEdge : repo.getNodeDAO().getAll()) {
+			for (Node potentialEdge : mainController.queueManager.getCurrentNodeChanges()) {
 				if (calcWeight(potentialEdge) < 60) edgeField.getItems().add(potentialEdge.getNodeID());
 			}
 		} else {
 			edgeField.setPromptText("All Nodes");
-			for (Node potentialEdge : repo.getNodeDAO().getAll())
+			for (Node potentialEdge : mainController.queueManager.getCurrentNodeChanges())
 				edgeField.getItems().add(potentialEdge.getNodeID());
 		}
 	}
 
 	public void setInfo(Node node) {
 		this.node = node;
-		addedEdges.clear();
-		addedMoves.clear();
-		update();
 		nodeIDLabel.clear();
 		edgeBox.getChildren().clear();
 		moveBox.getChildren().clear();
@@ -155,18 +142,28 @@ public class EditNodePopOverController {
 		nodeIDLabel.setText(String.valueOf(node.getNodeID()));
 		if (mainController.queueManager.getCurrentEdgeChanges() != null)
 			for (Edge edge : mainController.queueManager.getCurrentEdgeChanges()) {
-				int id;
+				int id = -1;
 				if (edge.getStartNodeID() == node.getNodeID()) id = edge.getEndNodeID();
-				else id = edge.getStartNodeID();
-				edgeBox.getChildren().add(new Label(String.valueOf(id)));
+				if (edge.getEndNodeID() == node.getNodeID()) id = edge.getStartNodeID();
+				if (id != -1)
+					edgeBox.getChildren().add(new Label(String.valueOf(id)));
 			}
 		if (repo.getLocationsAtNode(node) != null) {
 			List<String> sortedLocations = new ArrayList<>();
-			for (Move move : repo.getMoveDAO().getLocationsAtNodeID().get(node.getNodeID())) {
-				Label label = new Label();
-				label.setText(move.getLocationName() + " on " + move.getDate().toString());
+			for (Move move : mainController.queueManager.getCurrentMoveChanges()) {
+				if (move.getNodeID() != node.getNodeID()) continue;
+				Label label = new Label(move.getLocationName() + " on " + move.getDate().toString());
+				labelToMove.put(label, move);
 				label.setWrapText(true);
 				label.maxWidth(70);
+				label.setOnMouseClicked(event -> {
+					if (event.isControlDown()) {
+						Move temp = labelToMove.get(label);
+						mainController.queueManager.addToDeleteQueue(temp);
+						labelToMove.remove(label);
+						moveBox.getChildren().remove(label);
+					}
+				});
 				moveBox.getChildren().add(label);
 				sortedLocations.add(
 						move.getLocationName() + " - " + move.getLocation().getNodeType().toString());
